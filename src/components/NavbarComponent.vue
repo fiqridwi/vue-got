685b15a7ee3085c213db9daae2ca54224e29611e
DESKTOP-MD0FP6C\Ahady <nurfiqri23@gmail.com>
2022-05-20 10:05:08 +0700

    initial commit

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <v-app-bar app color="black" dark>
+      <div class="d-flex align-center">
+        <v-icon>mdi-store</v-icon><span class="mr-5">App</span>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Vuetify</span>
+      </v-btn>
+    </v-app-bar>
+  </div>
+</template>
